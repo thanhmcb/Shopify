5bd6876c2e2f45c0ac3c93b595b3b0ca3f54adb7
Kim Thanh <thanh@mcb.vn>
2024-04-02 13:58:14 +0700

    upgraded to new version

---
+++ Bikeworld-156215935307/assets/wishList.js
@@ -0,0 +1,166 @@
+const LOCAL_STORAGE_WISHLIST_KEY = 'shopify-wishlist';
+const LOCAL_STORAGE_DELIMITER = ',';
+const BUTTON_ACTIVE_CLASS = 'active';
+const GRID_LOADED_CLASS = 'loaded';
+
+const selectors = {
+  button: '[button-wishlist]',
+  grid: '[grid-wishlist]',
+  productCard: '.product-item',
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initButtons();
+  initGrid();
+});
+
+function addWishlistCount(wishlist) {
+    $('.wishlist--link__count').attr("data-wishlist-count",wishlist.length).html(wishlist.length);
+}
+
+document.addEventListener('shopify-wishlist:updated', (event) => {
+  console.log('[Shopify Wishlist] Wishlist Updated ✅', event.detail.wishlist);
+  initGrid();
+  addWishlistCount(event.detail.wishlist);
+});
+
+document.addEventListener('shopify-wishlist:init-product-grid', (event) => {
+  console.log('[Shopify Wishlist] Wishlist Product List Loaded ✅', event.detail.wishlist);
+});
+
+document.addEventListener('shopify-wishlist:init-buttons', (event) => {
+  console.log('[Shopify Wishlist] Wishlist Buttons Loaded ✅', event.detail.wishlist);
+  addWishlistCount(event.detail.wishlist);
+});
+
+const fetchProductCardHTML = (handle) => {
+  const productTileTemplateUrl = `/products/${handle}?view=card`;
+  return fetch(productTileTemplateUrl)
+  .then((res) => res.text())
+  .then((res) => {
+    const text = res;
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(text, 'text/html');
+    const productCard = htmlDocument.documentElement.querySelector(selectors.productCard);
+    return productCard.outerHTML;
+  })
+  .catch((err) => console.error(`[Shopify Wishlist] Failed to load content for handle: ${handle}`, err));
+};
+
+const setupGrid = async (grid) => {
+  const wishlist = getWishlist();
+  const requests = wishlist.map(fetchProductCardHTML);
+  const responses = await Promise.all(requests);
+  const wishlistProductCards = responses.join('');
+  grid.innerHTML = wishlistProductCards;
+  grid.classList.add(GRID_LOADED_CLASS);
+  initButtons();
+
+  const event = new CustomEvent('shopify-wishlist:init-product-grid', {
+    detail: { wishlist: wishlist }
+  });
+  document.dispatchEvent(event);
+};
+
+const setupButtons = (buttons) => {
+  buttons.forEach((button) => {
+    const productHandle = button.dataset.productHandle || false;
+    if (!productHandle) return console.error('[Shopify Wishlist] Missing `data-product-handle` attribute. Failed to update the wishlist.');
+    if (wishlistContains(productHandle)) button.classList.add(BUTTON_ACTIVE_CLASS);
+    button.addEventListener('click', () => {
+      updateWishlist(productHandle);
+      button.classList.toggle(BUTTON_ACTIVE_CLASS);
+    });
+  });
+};
+
+const initGrid = () => {
+  const grid = document.querySelector(selectors.grid) || false;
+  if (grid) setupGrid(grid);
+};
+
+const initButtons = () => {
+  const buttons = document.querySelectorAll(selectors.button) || [];
+  if (buttons.length) setupButtons(buttons);
+  else return;
+  const event = new CustomEvent('shopify-wishlist:init-buttons', {
+    detail: { wishlist: getWishlist() }
+  });
+  document.dispatchEvent(event);
+};
+
+const getWishlist = () => {
+  const wishlist = localStorage.getItem(LOCAL_STORAGE_WISHLIST_KEY) || false;
+  if (wishlist) return wishlist.split(LOCAL_STORAGE_DELIMITER);
+  return [];
+};
+
+const setWishlist = (array) => {
+  const wishlist = array.join(LOCAL_STORAGE_DELIMITER);
+  if (array.length) localStorage.setItem(LOCAL_STORAGE_WISHLIST_KEY, wishlist);
+  else localStorage.removeItem(LOCAL_STORAGE_WISHLIST_KEY);
+
+  const event = new CustomEvent('shopify-wishlist:updated', {
+    detail: { wishlist: array }
+  });
+  document.dispatchEvent(event);
+
+  return wishlist;
+};
+
+const updateWishlist = (handle) => {
+  const wishlist = getWishlist();
+  const indexInWishlist = wishlist.indexOf(handle);
+  if (indexInWishlist === -1) wishlist.push(handle);
+  else wishlist.splice(indexInWishlist, 1);
+  return setWishlist(wishlist);
+};
+
+const wishlistContains = (handle) => {
+  const wishlist = getWishlist();
+  return wishlist.includes(handle);
+};
+
+const resetWishlist = () => {
+  return setWishlist([]);
+};
+
+
+//# wishlist share
+
+const getWishlistURL = () => {
+    const wishlist = localStorage.getItem(LOCAL_STORAGE_WISHLIST_KEY) || false;
+    if (wishlist) return wishlist.split(LOCAL_STORAGE_DELIMITER);
+    return [];
+};
+$(document).on('click', '.button-wishlist-share', function (event) {
+    var currentURL = window.location.href;
+    var datahandle = "?datahandle=";
+    const wishlist = datahandle + getWishlistURL();
+    const wishlistURL = currentURL + wishlist.replaceAll(",","&datahandle=");
+    console.log('test'+ wishlistURL);
+   document.getElementById("wishlist-link").value = wishlistURL;
+});
+
+    function copyToClipboard() {
+        const copyText = document.getElementById("wishlist-link").value;
+        let input = document.createElement('input');
+        input.style.display = 'none';
+        document.body.appendChild(input);
+        input.value = copyText;
+        input.focus();
+        input.select();
+        input.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(input.value);
+        alert("Link kopieret: " + input.value);
+    }
+
+
+var clipboard = new ClipboardJS('#btnCopyToClipboard');
+clipboard.on('success', function (e) {
+	console.log('wishList url copied successfully to clipboard');
+});
+
+clipboard.on('error', function (e) {
+	console.log(e);
+});
